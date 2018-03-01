10205c0634773d393e6d5bb2399708e19920f779
Noah Murad <noahmurad@hotmail.com>
2018-02-28 22:40:13 -0500

    Note detection fully working for trivial example

--- rion.py
+++ rion.py
@@ -4,41 +4,32 @@ import matplotlib.pyplot as plt
 import math
 import peakutils
 import sys
+import ltfatpy
 
 import pdb
 
-import note as note_lib
+import note as notepy
 
-fs = 44100
-
-TRUMPET_SAMPLE = 'samples/Trumpet.wav'
-TRUMPET_1H_LOW = 800
-TRUMPET_1H_HIGH = 1200
-
-OBOE_SAMPLE = 'samples/Oboe.wav'
-OBOE_NOTE1_END = 70000
-OBOE_1H_LOW = 300
-OBOE_1H_HIGH = 500
-
-def note_filter(x):
+def note_filter(x, fs=44100):
     '''
-    Transfers signal in the time domain into musical note buckets
+    Transfers a signal in the time domain into musical note buckets
 
-    X_notes, notes = note_filter(X)
+    X_notes, notes = note_filter(x)
 
     input
         x: signal in time domain
 
     output
         X_notes: signal in frequency domain with only musical note buckets
-        notes: array of Note objects of len(X_notes) denoting each corresponding note to each bucket in X_notes
+        notes: array of Note objects of len(X_notes) 
+            denoting each corresponding note to each bucket in X_notes
     '''
 
     # Get the list of notes that we will bucket to
-    notes = note_lib.note_range(start=note_lib.Note('A', 0), end=note_lib.Note('F', 9))
+    notes = notepy.note_range(start=notepy.Note('A', 0), end=notepy.Note('F', 9))
 
     # Get the edge frequencies of each note
-    note_edges = note_lib.note_range(start=note_lib.Note('A', 0), end=note_lib.Note('F#', 9))
+    note_edges = notepy.note_range(start=notepy.Note('A', 0), end=notepy.Note('F#', 9))
     for note_edge in note_edges:
         note_edge.shift(-0.5)
     note_edges = list(map(lambda n: n.frequency, note_edges))
@@ -48,52 +39,20 @@ def note_filter(x):
 
     # DFT
     X = np.real(np.fft.fft(x))
+    X = X[:len(X//2)]
 
     # Fill note buckets
     for i in range(len(notes)):
         note = notes[i]
-        edge_low = note_edges[i]
-        edge_high = note_edges[i+1]
+        edge_low = note_edges[i] * len(x)/fs
+        edge_high = note_edges[i+1] * len(x)/fs
 
         # Iterate through frequencies near the current note
         for freq in range(math.floor(edge_low), math.ceil(edge_high)+1):
             similarity = 1/(abs(note.frequency - freq)+1)**(1/12)
-            X_notes[i] += abs(X[freq]) * similarity
-            if X_notes[i] < 0:
-                pass
-                #pdb.set_trace()
+            X_notes[i] += (abs(X[freq]))**2 * similarity
+
+        # Normalize to the size (width) of the bucket
+        X_notes[i] /= (edge_high - edge_low)
 
     return X_notes, notes
-
-if __name__ == '__main__':
-    sample_file = TRUMPET_SAMPLE
-    
-    if len(sys.argv) > 1:
-        sample_file = sys.argv[1]
-
-    # Load trumpet sample
-    rate, x = scipy.io.wavfile.read(sample_file)
-    X = np.fft.fft(x)
-    plt.plot(X)
-    plt.show()
-
-    X_notes, notes = note_filter(x)
-    X_notes = X_notes / max(X_notes)
-
-    peaks = peakutils.indexes(X_notes, thres=0.02/max(X_notes), min_dist=1)
-
-    plt.plot(X_notes)
-    plt.scatter(peaks, np.zeros(len(peaks)))
-    plt.show()
-
-    peak_notes = [notes[peak] for peak in peaks]
-    f_0 = peak_notes[0]
-    print(peak_notes)
-
-    peak_tones = [peak_note.name for peak_note in peak_notes]
-    modal_tone = max(set(peak_tones), key=peak_tones.count)
-
-    print('f_0 is {} and modal tone is {}'.format(f_0, modal_tone))
-
-    # Save result
-    #scipy.io.wavfile.write('TrumpetOut.wav', fs, x)
